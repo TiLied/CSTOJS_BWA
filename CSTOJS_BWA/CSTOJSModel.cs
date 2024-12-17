9b52d352130c05523e2c26fcd6820c3b823c98ce
TiLied <TiLied@users.noreply.github.com>
2024-12-17 17:58:15 +0300

    Add project files.

---
+++ CSTOJS_BWA/CSTOJSModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CSTOJS_BWA
+{
+	using System.ComponentModel.DataAnnotations;
+
+	public class CSTOJSModel
+	{
+		[Required]
+		public string CSString { get; set; } = string.Empty;
+
+		public string JSString { get; set; } = string.Empty;
+
+		public bool Display { get; set; } = true;
+	}
+}
