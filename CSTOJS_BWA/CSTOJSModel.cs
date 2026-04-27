d99a99e9af18a44ad6748b12951595ebbf06a30c
TiLied <TiLied@users.noreply.github.com>
2026-04-27 19:53:06 +0300

    Added support for the "DisableDiagnostics" and "EnableModules" options. Note: Because there is only one file, you can only force (EnableModules = 2) and only export classes. Changed layout.

--- CSTOJS_BWA/CSTOJSModel.cs
+++ CSTOJS_BWA/CSTOJSModel.cs
@@ -1,29 +1,29 @@
-﻿namespace CSTOJS_BWA
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSTOJS_BWA;
+
+public class CSTOJSModel
 {
-	using System.Collections.Generic;
-	using System;
-	using System.ComponentModel.DataAnnotations;
-	using System.Text;
+	[Required]
+	public string CSString { get; set; } = string.Empty;
 
-	public class CSTOJSModel
-	{
-		[Required]
-		public string CSString { get; set; } = string.Empty;
+	public string JSString { get; set; } = string.Empty;
 
-		public string JSString { get; set; } = string.Empty;
-
-		public bool Display { get; set; } = true;
+	public bool Display { get; set; } = true;
 
 
-		public bool Debug { get; set; } = false;
-		public bool DisableConsoleOutput { get; set; } = false;
-		public bool UseVarOverLet { get; set; } = false;
-		public bool KeepBraceOnTheSameLine { get; set; } = false;
-		public bool NormalizeWhitespace { get; set; } = false;
-		public bool MakePropertiesEnumerable { get; set; } = false;
+	public bool Debug { get; set; } = false;
+	public bool DisableConsoleOutput { get; set; } = false;
+	public bool DisableDiagnostics { get; set; } = false;
+	public bool UseVarOverLet { get; set; } = false;
+	public bool KeepBraceOnTheSameLine { get; set; } = false;
+	public bool NormalizeWhitespace { get; set; } = false;
+	public bool MakePropertiesEnumerable { get; set; } = false;
 
-		public string CustomCSNamesToJS { get; set; } = string.Empty;
-		public string AddSBAtTheTop { get; set; } = string.Empty;
-		public string AddSBAtTheBottom { get; set; } = string.Empty;
-	}
+	public int EnableModules { get; set; } = 1;
+
+	public string CustomCSNamesToJS { get; set; } = string.Empty;
+	public string AddSBAtTheTop { get; set; } = string.Empty;
+	public string AddSBAtTheBottom { get; set; } = string.Empty;
 }
+
